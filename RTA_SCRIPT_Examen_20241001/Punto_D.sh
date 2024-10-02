f66d1800e4f1044453b77d0f961442d355e0dc1e
noecoro <coronoelia@gmail.com>
2024-10-02 17:10:59 -0400

    Estructura- script del Punto D

---
+++ RTA_SCRIPT_Examen_20241001/Punto_D.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#osboxes@osboxes:~$ mkdir -p Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},clientes/cartas_{1..100}}
+#osboxes@osboxes:~$ tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --columns=4
+mkdir -p Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},clientes/cartas_{1..100}}
+tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --columns=4
+echo "Punto_D ok"
