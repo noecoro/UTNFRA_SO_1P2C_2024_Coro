3a0e5ad5a1238419aef05f90802851e5e0042e84
noecoro <coronoelia@gmail.com>
2024-10-06 18:24:39 -0400

    fix(scripts): corregir rutas de archivos en Punto_E.sh y Punto_F.sh

--- RTA_SCRIPT_Examen_20241001/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_E.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 echo
 echo "Filtro Basico Punto_E: "
-# Crear los directorios si no existen 01 - 02 (error)
-mkdir -p RTA_ARCHIVOS_Examen_20241001
 
-# Crear el archivo con la información de la memoria y el chasis
-grep MemTotal /proc/meminfo > RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
+# Crear los directorios si no existen (error corregido)
+mkdir -p ../RTA_ARCHIVOS_Examen_20241001
+
+# Crear el archivo  la memoria y el chasis
+grep MemTotal /proc/meminfo > ../RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
 
 # Agregar título "Chassis Information"
-echo "Chassis Information" >> RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
+echo "Chassis Information" >> ../RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
 
 # Agregar fabricante del chasis
-sudo dmidecode -t chassis | grep "Manufacturer" >> RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep "Manufacturer" >> ../RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
 echo
 echo "Muestro archivo: "
-cat RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
+cat ../RTA_ARCHIVOS_Examen_20241001/Filtro_Basico.txt
 echo
 
