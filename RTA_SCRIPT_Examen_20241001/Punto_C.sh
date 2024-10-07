62337375ca4ccc9d47c2e2a7aa8ca7791741193b
noecoro <coronoelia@gmail.com>
2024-10-06 21:05:45 -0400

    fix(Punto_C): corregido error permisos

--- RTA_SCRIPT_Examen_20241001/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_C.sh
@@ -38,15 +38,15 @@ sudo chmod -R 750 /Examenes-UTN/alumno_1
 
 # Para /Examenes-UTN/alumno_2
 sudo chown -R p1c2_2024_A2:p1c2_2024_gAlumno /Examenes-UTN/alumno_2
-sudo chmod -R 700 /Examenes-UTN/alumno_2
+sudo chmod -R 760 /Examenes-UTN/alumno_2
 
 # Para /Examenes-UTN/alumno_3
 sudo chown -R p1c2_2024_A3:p1c2_2024_gAlumno /Examenes-UTN/alumno_3
-sudo chmod -R 770 /Examenes-UTN/alumno_3
+sudo chmod -R 700 /Examenes-UTN/alumno_3
 
 # Para /Examenes-UTN/profesores
 sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
-sudo chmod -R 750 /Examenes-UTN/profesores
+sudo chmod -R 775 /Examenes-UTN/profesores
 
 # Creo archivo validar.txt en cada carpeta
 echo "Creo archivo validar.txt en cada carpeta"
